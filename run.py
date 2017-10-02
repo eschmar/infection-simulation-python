55e458bcb5b528aa54bdfac9bd5dd576a14f30ce
Marcel Eschmann <marcel.e@nectima.se>
2017-10-02 10:58:16 +0200

    Parse input parameters and override default values

--- run.py
+++ run.py
@@ -4,8 +4,26 @@ import sys
 
 params = helper.parseArguments(sys.argv)
 
+# default values
+size = 40
+probabilityOfInfection = 0.02
+probabilityOfDeath = 0
+infectionLength = [6, 9]
+
+if '-N' in params and params['-N']:
+    size = params['-N']
+
+if '-S' in params and params['-S']:
+    probabilityOfInfection = params['-S']
+
+if '-L' in params and params['-L']:
+    probabilityOfDeath = params['-L']
+
+if '-minDays' in params and params['-minDays'] and '-maxDays' in params and params['-maxDays']:
+    infectionLength = [params['-minDays'], params['-maxDays']]
+
 # init simulation
-simulation = Simulation(40, 0.02, 0, [6, 9])
+simulation = Simulation(size, probabilityOfInfection, probabilityOfDeath, infectionLength)
 if '--silent' in params and params['--silent']:
     simulation.isSilent = True
 if '--frames' in params and params['--frames']:
